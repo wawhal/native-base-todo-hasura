d712474cce116202fb3c92b827ea6dee196b0601
wawhal <rishichandra.wawhal@gmail.com>
2018-01-09 18:07:19 +0530

    Updated GIF

--- react-native/src/components/TodoItem.js
+++ react-native/src/components/TodoItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text, Icon, ListItem, CheckBox } from 'native-base';
+import { Text, Icon, ListItem, Body, CheckBox } from 'native-base';
 
 const TodoItem = ({ toggle, remove, item }) => (
   <ListItem style={{ flex: 1 }}>
     <CheckBox onPress={toggle} checked={item.completed} />
-    <Text style={{ alignSelf: 'center' }}>
-      {item.text}
-    </Text>
+    <Body>
+      <Text style={{ alignSelf: 'center' }}>
+        {item.text}
+      </Text>
+    </Body>
     <Icon name="md-trash" style={{ color: '#000000' }} onPress={remove} />
   </ListItem>
 );
